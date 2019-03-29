906c93de8d540b8ff5c5b7ec6014dc5c235c4f92
Max <max.ronald9@gmail.com>
2019-03-29 20:00:38 +0300

    New_classes

--- menu.cpp
+++ menu.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect (ui->Row, SIGNAL(currentIndexChanged(int)), this, SLOT (on_Row_currentIndexChanged (int)));
+    connect (ui->Column, SIGNAL(currentIndexChanged(int)), this, SLOT (on_Column_currentIndexChanged (int)));
 }
 
 MainWindow::~MainWindow()
@@ -13,7 +15,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_Row_currentIndexChanged(int index)
 {
+    Controller_matrix m_r;
+    m_r.rows = index;
+    m_r.get_rows();
 }
+
+void MainWindow::on_Column_currentIndexChanged(int index)
+{
+    Controller_matrix m_c;
+    m_c.columns = index;
+    m_c.get_columns();
+}
+
+
+int Controller_matrix::get_rows()
+{
+    for (int i = 2; i < rows; i++)
+    {
+
+    }
+    return 0;
+}
+
+int Controller_matrix::get_columns()
+{
+    for (int i = 2; i < columns; i++)
+    {
+
+    }
+    return 0;
+}
+
--- menu.h
+++ menu.h
@@ -2,10 +2,17 @@
 #define MENU_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QComboBox>
+#include <QLineEdit>
 
 namespace Ui {
 class MainWindow;
+class Controller_matrix;
 }
+
+class Model_matrix;
+class View_matrix;
 
 class MainWindow : public QMainWindow
 {
@@ -14,12 +21,23 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private slots:
-    void on_pushButton_clicked();
+    void on_Row_currentIndexChanged(int index);
+    void on_Column_currentIndexChanged(int index);
 
 private:
     Ui::MainWindow *ui;
+};
+
+class Controller_matrix : public QMainWindow
+{
+public:
+    int rows;
+    int columns;
+    Controller_matrix () : rows(0) , columns (0)
+    {}
+    int get_rows();
+    int get_columns();
 };
 
 #endif // MENU_H
